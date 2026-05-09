19b6bf3198f8c3fa06d4c98a71d6dcd1c0a3d4d9
Taras <106152553+TarasYurkovskyi@users.noreply.github.com>
2026-05-09 22:24:47 +0300

    Feature/pz 1 (#1)

---
+++ pz1/src/main/java/org/pz1/domain/Category.java
@@ -0,0 +1,11 @@
+package org.pz1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Category {
+    private int id;
+    private String name;
+}
---
+++ pz1/src/main/java/org/pz1/domain/Product.java
@@ -0,0 +1,14 @@
+package org.pz1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private String description;
+    private Category category;
+}
